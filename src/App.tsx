0c77c80652bd7960dfb91d0ebf9390a226948edd
kaleb chara <kalebdavidchara@hotmail.com>
2022-07-03 08:35:05 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import Bootstrap from "./components/Bootstrap";
+import Bulma from "./components/Bulma";
+import ReactBoostrap from "./components/ReactBoostrap";
+import MaterialUI from './components/MaterialUI';
+
+
+
+function App() {
+  return (
+    <div>
+      <h2>Frameworks CSS con React</h2>
+      {/* <Bootstrap/> */}
+      {/* <Bulma/> */}
+      {/* <ReactBoostrap/> */}
+      <MaterialUI/>
+    </div>
+  );
+}
+
+export default App;
